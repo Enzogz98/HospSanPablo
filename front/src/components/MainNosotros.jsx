b00cab0fb3a83114341cbc541de1ca6c8b5964ed
Enzogz98 <enzo.114998@gmail.com>
2024-02-07 01:18:02 -0300

    about us hecho

--- front/src/components/MainNosotros.jsx
+++ front/src/components/MainNosotros.jsx
@@ -1,11 +1,66 @@
 import React from "react";
+import '../Css/aboutUs.css'
 
 const MainNosotros = () => {
   return (
     <div className="container">
       <div className="container-about">
-        <h1>Hospital de San Pablo</h1>
-        <p></p>
+        <h1>Ingenio San Pablo</h1>
+        <p>
+          San Pablo es una localidad argentina ubicada en el departamento Lules
+          de la provincia de Tucumán. Se encuentra al pie de las Sierras de San
+          Javier. La localidad giró en torno a la actividad del ingenio homónimo
+          hasta que este cerró en 1989. Se halla a 13 km al sudoeste de San
+          Miguel de Tucumán, en la zona de llanura contigua a las Sierras de San
+          Javier. El municipio se denomina San Pablo y Villa Nougués por abarcar
+          también la zona de montaña contigua en la que se erige la población de
+          Villa Nougués, con origen como caserío de los dueños del ingenio.
+        </p>
+        <h1>Historia</h1>
+        <p>
+          Tras la llegada de los españoles Melián de Leguizamón se convirtió en
+          la primera propietaria de estas tierras en 1592. En 1670 la Compañía
+          de Jesús adquirió las mismas a su nieta Jordana de Trejo Leguizamón.
+          En 1767 con la expulsión de los jesuitas ya figuraba la Estancia de
+          San Pablo, con una extensión que contenía El Manantial y Las Sierras
+          de San Javier, así como también existía un asentamiento poblacional.
+          En el monte circundante se extraía madera para la fabricación de
+          carretas. José Domingo Ayala compró las tierras y en 1826 las vendió a
+          Jean Nougués, ciudadano francés. El 3 de abril de 1821 se produjo
+          una contienda entre las tropas tucumanas encabezadas por el coronel
+          mayor Bernabé Aráoz y el coronel Abraham González contra las tropas
+          lideradas por el entonces militar tucumano Alejandro Heredia, en el
+          manantial de Marlopa, próximo al obraje de San Pablo con la victoria
+          de los primeros. El 24 de agosto del mismo año también en el rincón de
+          Marlopa se enfrentaron las fuerzas de Javier López y Bernabé Aráoz, en
+          las cuales venció López y tomó control del gobierno. En 1832 Nougués
+          instaló el ingenio San Pablo con un primitivo trapiche de madera, pero
+          el lugar definitivo recién llegaría en 1855, cuando debió ser mudado a
+          un mejor terreno y alejado de la zona de acampe de tropas invasoras.
+          Nougués añadió al ingenio otras actividades como una curtiduría,
+          aserradero y un molino harinero. Formalmente se considera fundado el
+          pueblo de San Pablo cuando se levantaron las primeras casas para el
+          personal trabajador del ingenio, aproximadamente en el año 1860. En
+          1875 llegó un contingente de 38 familias de indios prisioneras de la
+          zona de la actual Provincia del Chaco, quienes se instalaron en el
+          lugar y contribuyeron al desarrollo del ingenio y el poblado. En
+          1876 llegó el ferrocarril a San Miguel de Tucumán y el ingenio lo
+          aprovechó para modernizarse, en 1882 por ejemplo instalaron las
+          primeras máquinas de vapor. En 1911 se fundó el club del pueblo,
+          pero su estadio debió ser trasladado a su actual ubicación en 1919. En
+          la década siguiente se creó el hospital. El templo católico de Nuestra
+          Señora del Pilar se edificó en 1941. En 1975 la localidad sufrió el
+          terrorismo de Estado, entre otros crímenes de lesa humanidad llevaron
+          detenidos ilegalmente a decenas de personas en un casamiento. Este
+          caso fue juzgado en el marco del Juicio del Operativo Independencia
+          I. En 1987 varios factores llevaron a la quiebra del ingenio.
+          Durante dos años estuvo a cargo de CATSA, un grupo de cañeros, pero
+          luego permaneció cerrado hasta 1992. Dicho año el ingenio abrió
+          temporalmente, hasta que debió suspender las actividades con el corte
+          del suministro de gas. En 1996 se anunció el desguace definitivo, el
+          cual se finalizó aun con la tenaz resistencia de los vecinos de San
+          Pablo.
+          </p>
       </div>
     </div>
   );
