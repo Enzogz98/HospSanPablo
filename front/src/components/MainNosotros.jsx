05a2675f565af0901274b00b5980f15508e8ab32
Enzogz98 <enzo.114998@gmail.com>
2024-02-07 01:10:13 -0300

    contacto terminado

---
+++ front/src/components/MainNosotros.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MainNosotros = () => {
+  return (
+    <div className="container">
+      <div className="container-about">
+        <h1>Hospital de San Pablo</h1>
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default MainNosotros;
